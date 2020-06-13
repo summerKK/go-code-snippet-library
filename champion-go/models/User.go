20ee0ef33fe52fc8c79cd814c59a63568496e239
Summer <su943515688@gmail.com>
2020-06-13 18:46:14 +0800

    [+] user model

---
+++ champion-go/models/User.go
@@ -0,0 +1,161 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"html"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Nickname  string    `gorm:"size:255;not null;unique" json:"nickname"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword string, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (u *User) BeforeSave() error {
+	hashedPassword, err := Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+
+	return nil
+}
+
+func (u *User) Prepare() {
+	u.ID = 0
+	u.Nickname = html.EscapeString(strings.TrimSpace(u.Nickname))
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+}
+
+func (u *User) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "update":
+		if u.Nickname == "" {
+			return errors.New("Required Nickname")
+		}
+		if u.Password == "" {
+			return errors.New("Required Password")
+		}
+		if u.Email == "" {
+			return errors.New("Required Email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("Invalid Email")
+		}
+
+		return nil
+	case "login":
+		if u.Password == "" {
+			return errors.New("Required Password")
+		}
+		if u.Email == "" {
+			return errors.New("Required Email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("Invalid Email")
+		}
+		return nil
+
+	default:
+		if u.Nickname == "" {
+			return errors.New("Required Nickname")
+		}
+		if u.Password == "" {
+			return errors.New("Required Password")
+		}
+		if u.Email == "" {
+			return errors.New("Required Email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("Invalid Email")
+		}
+		return nil
+	}
+}
+
+func (u *User) SaveUser(db *gorm.DB) (*User, error) {
+
+	var err error
+	err = db.Debug().Create(&u).Error
+	if err != nil {
+		return &User{}, err
+	}
+	return u, nil
+}
+
+func (u *User) FindAllUsers(db *gorm.DB) (*[]User, error) {
+	var err error
+	var users []User
+	err = db.Debug().Model(&User{}).Limit(100).Find(&users).Error
+	if err != nil {
+		return &[]User{}, err
+	}
+	return &users, err
+}
+
+func (u *User) FindUserById(db *gorm.DB, uid uint32) (user *User, err error) {
+	user = &User{}
+	err = db.Debug().Model(User{}).Where("id = ?", uid).Take(user).Error
+	if err != nil {
+		return
+	}
+	if gorm.IsRecordNotFoundError(err) {
+		return nil, fmt.Errorf("User not found(user_id:%d)", uid)
+	}
+
+	return
+}
+
+func (u *User) UpdateAUser(db *gorm.DB, uid uint32) (user *User, err error) {
+	// hash password
+	err = u.BeforeSave()
+	if err != nil {
+		log.Fatal(err)
+	}
+	user = &User{}
+	db = db.Model(User{}).Where("id = ?", uid).Take(user).UpdateColumns(
+		map[string]interface{}{
+			"password":  u.Password,
+			"nickname":  u.Nickname,
+			"email":     u.Email,
+			"update_at": time.Now(),
+		},
+	)
+	if db.Error != nil {
+		err = db.Error
+		return
+	}
+	err = db.Debug().Model(&User{}).Where("id = ?", uid).Take(user).Error
+
+	return
+}
+
+func (u *User) DeleteAUser(db *gorm.DB, uid uint32) (rowAffected int64, err error) {
+	db = db.Debug().Model(User{}).Where("id = ?", uid).Take(&User{}).Delete(&User{})
+	if db.Error == nil {
+		rowAffected = db.RowsAffected
+	}
+
+	return
+}
