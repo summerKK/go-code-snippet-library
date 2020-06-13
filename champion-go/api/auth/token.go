78a260980a3d3715b29f1907249eaa42f3b7e80a
Summer <su943515688@gmail.com>
2020-06-13 20:02:03 +0800

    [+] response && token validate

---
+++ champion-go/api/auth/token.go
@@ -0,0 +1,96 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func CreateToken(userId uint32) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["authorized"] = true
+	claims["user_id"] = userId
+	claims["exp"] = time.Now().Add(time.Hour).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("API_SECRET")))
+}
+
+func ExtractToken(r *http.Request) string {
+	keys := r.URL.Query()
+	token := keys.Get("token")
+	if token != "" {
+		return token
+	}
+
+	bearerToken := r.Header.Get("Authorization")
+	split := strings.Split(bearerToken, " ")
+	if len(split) == 2 {
+		return split[1]
+	}
+
+	return ""
+}
+
+func TokenValid(r *http.Request) error {
+	tokenStr := ExtractToken(r)
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+	if err != nil {
+		return err
+	}
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		Pretty(claims)
+	}
+
+	return nil
+}
+
+// Pretty display the claims licely in the terminal
+func Pretty(data interface{}) {
+	b, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(string(b))
+}
+
+func ExtractTokenId(r *http.Request) (uint32, error) {
+	tokenStr := ExtractToken(r)
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(os.Getenv("API_SECRET")), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		uintUserId, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_id"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint32(uintUserId), nil
+	}
+
+	return 0, nil
+}
---
+++ champion-go/api/responses/json.go
@@ -0,0 +1,27 @@
+package responses
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "%s", err)
+	}
+}
+
+func Error(w http.ResponseWriter, statusCode int, err error) {
+	if err != nil {
+		JSON(w, statusCode, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		})
+	}
+
+	JSON(w, http.StatusBadRequest, nil)
+}
