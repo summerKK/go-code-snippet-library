9230b354b7e611a8a8f223ac5325a098d3dcd841
Summer <su943515688@gmail.com>
2020-07-17 00:22:16 +0800

    [+][blog-service] il8n多语言化

--- blog-service/internal/routers/router.go
+++ blog-service/internal/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/summerKK/go-code-snippet-library/blog-service/docs"
+	"github.com/summerKK/go-code-snippet-library/blog-service/internal/middleware"
 	v1 "github.com/summerKK/go-code-snippet-library/blog-service/internal/routers/api/v1"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -12,6 +13,8 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(middleware.Translations())
+
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	article := v1.NewArticle()
