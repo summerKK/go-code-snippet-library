3c6b6fc694a27ca3ffd729aee1a4a68dd0808237
Summer <su943515688@gmail.com>
2020-07-17 18:26:11 +0800

    [+][blog-service] article model

--- blog-service/internal/model/article.go
+++ blog-service/internal/model/article.go
@@ -1,6 +1,9 @@
 package model
 
-import "github.com/summerKK/go-code-snippet-library/blog-service/pkg/app"
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/summerKK/go-code-snippet-library/blog-service/pkg/app"
+)
 
 type ArticleSwagger struct {
 	List  []*Article
@@ -18,4 +21,46 @@ type Article struct {
 
 func (a Article) TableName() string {
 	return "blog_article"
+}
+
+func (a Article) Count(db *gorm.DB) (int, error) {
+	var count int
+	if a.Title != "" {
+		db = db.Where("title = ?", a.Title)
+	}
+	db = db.Where("state = ?", a.State)
+	if err := db.Model(&a).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (a Article) List(db *gorm.DB, pageSize, pageOffset int) ([]*Article, error) {
+	var articles []*Article
+	if pageSize >= 0 && pageOffset >= 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if a.Title != "" {
+		db = db.Where("title = ?", a.Title)
+	}
+	db = db.Where("state = ?", a.State)
+
+	if err := db.Where("is_del = ?", 0).Find(&articles).Error; err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
+
+func (a Article) Create(db *gorm.DB) error {
+	return db.Create(&a).Error
+}
+
+func (a Article) Update(db *gorm.DB, vales interface{}) error {
+	return db.Model(&a).Where("id = ? and is_del = ?", a.ID, 0).Updates(vales).Error
+}
+
+func (a Article) Delete(db *gorm.DB) error {
+	return db.Where("id = ? and is_del = ?", a.ID, 0).Delete(&a).Error
 }
