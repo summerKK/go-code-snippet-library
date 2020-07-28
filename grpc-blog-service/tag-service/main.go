bc9dd9c5d842786b877b3aba9be105a3841ea989
Summer <su943515688@gmail.com>
2020-07-28 17:34:45 +0800

    [+][grpc-blog-service-tag-service] 使用grpc-gateway实现 grpc和restfulAPi(http)

--- grpc-blog-service/tag-service/main.go
+++ grpc-blog-service/tag-service/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
-	"net"
 	"net/http"
+	"strings"
 
-	"github.com/soheilhy/cmux"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	pb "github.com/summerKK/go-code-snippet-library/grpc-blog-service/tag-service/proto"
 	tagServer "github.com/summerKK/go-code-snippet-library/grpc-blog-service/tag-service/server"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -21,36 +24,19 @@ func main() {
 	flag.StringVar(&port, "port", "8001", "服务启动端口")
 	flag.Parse()
 
-	tcpServer, err := RunTcpServer()
+	err := RunTcpServer()
 	if err != nil {
-		log.Fatalf("Run tcp server error:%v", err)
-	}
-
-	mux := cmux.New(tcpServer)
-	grpcL := mux.MatchWithWriters(cmux.HTTP2MatchHeaderFieldPrefixSendSettings("content-type", "application/grpc"))
-	httpL := mux.Match(cmux.HTTP1Fast())
-
-	grpcServer := RunGrpcServer()
-	httpServer := RunHttpServer()
-	go grpcServer.Serve(grpcL)
-	go httpServer.Serve(httpL)
-
-	err = mux.Serve()
-	if err != nil {
-		log.Fatalf("mux server run error:%v", err)
+		log.Fatalf("RunTcpServer error:%v", err)
 	}
 }
 
-func RunHttpServer() *http.Server {
+func RunHttpServer() *http.ServeMux {
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/ping", func(writer http.ResponseWriter, r *http.Request) {
 		_, _ = writer.Write([]byte("pong"))
 	})
 
-	return &http.Server{
-		Addr:    ":" + port,
-		Handler: serveMux,
-	}
+	return serveMux
 }
 
 func RunGrpcServer() *grpc.Server {
@@ -61,6 +47,32 @@ func RunGrpcServer() *grpc.Server {
 	return server
 }
 
-func RunTcpServer() (net.Listener, error) {
-	return net.Listen("tcp", ":"+port)
+func RunTcpServer() error {
+	httpMux := RunHttpServer()
+
+	// grpc service 服务提供 restful api支持
+	gatewayMux := RunGrpcGatewayServer()
+	httpMux.Handle("/", gatewayMux)
+
+	return http.ListenAndServe(":"+port, grpcHandleFunc(RunGrpcServer(), httpMux))
+}
+
+func RunGrpcGatewayServer() *runtime.ServeMux {
+	endpoint := "0.0.0.0:" + port
+	serveMux := runtime.NewServeMux()
+	options := []grpc.DialOption{grpc.WithInsecure()}
+	_ = pb.RegisterTagServiceHandlerFromEndpoint(context.Background(), serveMux, endpoint, options)
+
+	return serveMux
+}
+
+func grpcHandleFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 通过判断是 http1.1(http) http2(grpc) 走不通的handle处理请求
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
 }
--- grpc-blog-service/tag-service/scripts/proto.bash
+++ grpc-blog-service/tag-service/scripts/proto.bash
@@ -1,2 +1,4 @@
 # https://segmentfault.com/a/1190000021456180
-protoc --proto_path=../ --go_out=plugins=grpc,paths=source_relative:../ ../proto/*.proto
+#protoc --proto_path=../ --go_out=plugins=grpc,paths=source_relative:../ ../proto/*.proto
+
+protoc --proto_path=../ -I/Users/summer/go/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true,paths=source_relative:../ ../proto/*.proto
