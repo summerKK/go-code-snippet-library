38145daae6aeced5a560dcde1d8beb40b67cf71f
Summer <su943515688@gmail.com>
2020-09-01 10:41:00 +0800

    [+][chatroom] 用户添加token

--- chatroom/logic/user.go
+++ chatroom/logic/user.go
@@ -2,12 +2,19 @@ package logic
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"errors"
+	"fmt"
 	"io"
 	"regexp"
+	"strings"
 	"sync/atomic"
 	"time"
 
+	"github.com/spf13/cast"
+	"github.com/spf13/viper"
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
 )
@@ -22,6 +29,7 @@ type User struct {
 
 	conn *websocket.Conn
 
+	// 标识当前用户是否已经存在
 	isNew bool
 }
 
@@ -35,7 +43,6 @@ var regAtUsers = regexp.MustCompile(`@[^\s@]{2,20}`)
 
 func NewUser(nickname, token, addr string, conn *websocket.Conn) *User {
 	user := &User{
-		Uid:            int(atomic.AddUint64(&globalUid, 1)),
 		Nickname:       nickname,
 		EnterAt:        time.Now(),
 		Addr:           addr,
@@ -45,20 +52,57 @@ func NewUser(nickname, token, addr string, conn *websocket.Conn) *User {
 	}
 
 	// 已经登录过的用户
-	//if user.Token != "" {
-	//	uid, err := parseTokenAndValidate(token, nickname)
-	//	if err == nil {
-	//		user.Uid = uid
-	//	}
-	//}
-	//
-	//if user.Uid == 0 {
-	//	user.Uid = int(atomic.AddUint32(&globalUid, 1))
-	//	user.Token = genToken(user.Uid, user.Nickname)
-	//	user.isNew = true
-	//}
+	if user.Token != "" {
+		uid, err := parseTokenAndValidate(token, nickname)
+		if err == nil {
+			user.Uid = int(uid)
+		}
+	}
+
+	if user.Uid == 0 {
+		user.Uid = int(atomic.AddUint64(&globalUid, 1))
+		user.Token = genToken(user.Uid, user.Nickname)
+		user.isNew = true
+	}
 
 	return user
+}
+
+func parseTokenAndValidate(token string, nickname string) (uint64, error) {
+	pos := strings.LastIndex(token, "uid")
+	messageMac0, err := base64.StdEncoding.DecodeString(token[:pos])
+	if err != nil {
+		return 0, err
+	}
+	// pos + 3  去掉 `uid`  字符串
+	uid := cast.ToUint64(token[pos+3:])
+
+	secret := viper.GetString("token-secret")
+	message := fmt.Sprintf("%s%s%d", nickname, secret, uid)
+	messageMac1 := macSha256([]byte(message), []byte(secret))
+
+	// 验证token是否正确
+	ok := hmac.Equal(messageMac0, messageMac1)
+	if ok {
+		return uid, nil
+	}
+
+	return 0, errors.New("token is illegal")
+}
+
+func genToken(uid int, nickname string) string {
+	secret := viper.GetString("token-secret")
+	message := fmt.Sprintf("%s%s%d", nickname, secret, uid)
+	messageMac := macSha256([]byte(message), []byte(secret))
+
+	return fmt.Sprintf("%suid%d", base64.StdEncoding.EncodeToString(messageMac), uid)
+}
+
+func macSha256(message, secret []byte) []byte {
+	mac := hmac.New(sha256.New, secret)
+	mac.Write(message)
+
+	return mac.Sum(nil)
 }
 
 // 给用户发送消息
